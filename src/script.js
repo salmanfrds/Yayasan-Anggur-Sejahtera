ba39ced722917f131fb3cbefa02b46c900b3d7e0
salmanfrds <salmanfird4@outlook.com>
2024-09-08 15:28:17 +0800

    first commit

---
+++ src/script.js
@@ -0,0 +1,35 @@
+function shownav() {
+    const hambutton = document.getElementById("hamburger");
+    const menu = document.getElementById("mobile-menu");
+    hambutton.addEventListener("click", function() {
+        hambutton.classList.toggle("hamburger-active");
+        menu.classList.toggle("hidden");
+    });
+};
+  
+document.addEventListener("DOMContentLoaded", function() {
+    shownav();
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: 'true',
+      dynamicBullets: 'true'
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    autoplay: {
+        delay: 2000,
+    },
+});
