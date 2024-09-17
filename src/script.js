4905b504ac1ab61c56f5b92d2d1fb7e556939137
salmanfrds <salmanfird4@outlook.com>
2024-09-17 17:57:18 +0800

    update  17b

--- src/script.js
+++ src/script.js
@@ -1,39 +1,62 @@
 function shownav() {
-    const hambutton = document.getElementById("hamburger");
-    const menu = document.getElementById("mobile-menu");
-    hambutton.addEventListener("click", function() {
-        hambutton.classList.toggle("hamburger-active");
-        menu.classList.toggle("hidden");
-    });
+  const hambutton = document.getElementById("hamburger");
+  const menu = document.getElementById("mobile-menu");
+  hambutton.addEventListener("click", function() {
+      hambutton.classList.toggle("hamburger-active");
+      menu.classList.toggle("hidden");
+  });
 };
   
 document.addEventListener("DOMContentLoaded", function() {
-    shownav();
+  shownav();
 });
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+const swiperHome = new Swiper('.swiper-home', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
     
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: 'true',
-      dynamicBullets: 'true'
-    },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: 'true',
+    dynamicBullets: 'true'
+  },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 
-    autoplay: {
-        delay: 2000,
-    },
+  autoplay: {
+      delay: 2000,
+  },
+});
+
+const swiperProgram = new Swiper('.swiper-program', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
