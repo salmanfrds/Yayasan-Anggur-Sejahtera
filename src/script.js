3cd5ed31bb15a837f619eb0f4b140b7d3b58b443
salmanfrds <salmanfird4@outlook.com>
2024-09-19 17:29:19 +0800

    update 19a

--- src/script.js
+++ src/script.js
@@ -41,12 +41,14 @@ const swiperHome = new Swiper('.swiper-home', {
 const swiperProgram = new Swiper('.swiper-program', {
   // Optional parameters
   direction: 'horizontal',
-  loop: false,
+  loop: true,
   autoplay: false,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: 'true',
+    dynamicBullets: 'true'
   },
 
   // Navigation arrows
@@ -59,4 +61,21 @@ const swiperProgram = new Swiper('.swiper-program', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+    }
 });
