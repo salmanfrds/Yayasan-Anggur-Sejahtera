f8384ab6e7e93ba206e96a223d87df1691fc48b7
salmanfrds <salmanfird4@outlook.com>
2024-09-24 00:11:30 +0800

    update 24

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const app = express()
 app.listen(3000, () =>{
     console.log('Server running on http://localhost:3000');
 });
+
+app.get('/', (req, res) => {
+    res.send("hello from node api");
+})
