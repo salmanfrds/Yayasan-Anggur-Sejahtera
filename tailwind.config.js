847d10965dda73d031c54126f124b1e65eefc5d6
salmanfrds <salmanfird4@outlook.com>
2024-09-24 22:26:52 +0800

    update 24d

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.html"],
+  content: ['./public/**/*.{html,js}', './src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
